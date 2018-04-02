e2e03a53c9905027ba0599c9515f30fd60e98917
YasuChiba <yasu.bv0309@gmail.com>
2018-04-01 20:15:29 -0700

    no message

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,13 @@
+create table if  not exists testttt (
+  id int primary key,
+  name varchar(255),
+  age int,
+  sex tinyint
+);
+
+delete from testttt;
+insert into testttt;
+VALUES( 1 , 'John' , 26 , 1),
+  ( 2 , 'Bob' , 40 , 1),
+  ( 3 , 'Michael' , 20 , 1),
+  ( 4 , 'Mary' , 30 , 0);
